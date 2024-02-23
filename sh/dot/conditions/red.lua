0c6c432c2e8cba3be76d133f5e33762ae1c81fa0
Zoltan Levay <zoltan_levay@epam.com>
2024-02-23 22:20:21 +0100

    optional coloring

--- sh/dot/conditions/red.lua
+++ sh/dot/conditions/red.lua
@@ -1,14 +1,17 @@
 local events = {UNIT_POWER_FREQUENT}
 local condition = function()
-    local dpspredict = aura_env.calculateDps(aura_env.config.dot)
-    local dpsOnTarget = 0
-    local target = aura_env.dotTable[UnitGUID("target")]
-    if target then
-        if target[aura_env.dots[aura_env.config.dot].name] then
-            dpsOnTarget = target[aura_env.dots[aura_env.config.dot].name].dps
+    if aura_env.config.color then
+        local dpspredict = aura_env.calculateDps(aura_env.config.dot)
+        local dpsOnTarget = 0
+        local target = aura_env.dotTable[UnitGUID("target")]
+        if target then
+            if target[aura_env.dots[aura_env.config.dot].name] then
+                dpsOnTarget = target[aura_env.dots[aura_env.config.dot].name].dps
+            end
         end
+        local margin = 0.1
+        return dpsOnTarget > dpspredict + (dpsOnTarget * margin)
+    else
+        return false
     end
-
-    local margin = 0.1
-    return dpsOnTarget > dpspredict + (dpsOnTarget * margin)
 end
---
+++ sh/dot/options.lua
@@ -0,0 +1,2 @@
+aura_env.config.dot = 1 -- [1] = Shadow Word: Pain, [2] = Vampiric Touch
+aura_env.config.color = true
